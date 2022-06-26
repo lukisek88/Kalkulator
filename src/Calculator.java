8b6b581dbb77371385b10794b92c82b2bcaaf9cd
Lukasz Kudla <lukisek88@gmail.com>
2022-06-26 14:16:44 +0200

    version 0.1

---
+++ src/Calculator.java
@@ -0,0 +1,20 @@
+public class Calculator {
+
+
+    public double add(double a, double b) {
+        return a + b;
+    }
+
+    public double subtract(double a, double b) {
+        return a - b;
+    }
+
+    public static void main(String[] args) {
+        Calculator calc= new Calculator();
+        double add = calc.add(20,11.5);
+        double subtract = calc.subtract(20,11.5);
+
+        System.out.println("Wyniki: ");
+        System.out.println("Dodawanie: "+add+"  Odejmowanie: "+subtract);
+    }
+}
